ef892fe26a97bcd91b96557fee54729fe79b9753
Hack Reactor Student <student@hackreactor.com>
2017-05-19 15:51:44 -0700

    commit for set and graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,34 +2,66 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.vertices = [];
+  this.edges = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.vertices.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.vertices.includes(node);
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (var i = 0; i < this.edges.length; i++) {
+    if (this.edges[i].includes(node)) {
+      this.edges.splice(i, 1);
+    }
+  }
+  var index = this.vertices.indexOf(node);
+  this.vertices.splice(index, 1);
+  
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  var result = false;
+  for (var i = 0; i < this.edges.length; i++) {
+    if (this.edges[i][0] === fromNode && this.edges[i][1] === toNode) {
+      result = true;
+    }
+    if (this.edges[i][1] === fromNode && this.edges[i][0] === toNode) {
+      result = true; 
+    }
+  }
+  return result;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.edges.push([fromNode, toNode]);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  for (var i = 0; i < this.edges.length; i++) {
+    if ((this.edges[i][0] === fromNode && this.edges[i][1] === toNode) ||
+         this.edges[i][1] === fromNode && this.edges[i][0] === toNode) {
+      this.edges.splice(i, 1); 
+    }
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var i = 0; i < this.vertices.length; i++) {
+    cb(this.vertices[i]);
+  }
 };
 
 /*
